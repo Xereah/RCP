6bfbeca339fa3a313d2068da0609c5132e02d824
Xereah <patryk.struzik1398@gmail.com>
2025-11-20 10:06:56 +0100

    stworzenie projektu

---
+++ database/migrations/2025_11_20_090353_create_work_sessions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('work_sessions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('personel_id')->constrained('personel');
+            $table->date('work_date')->nullable();
+            $table->time('start_time')->nullable();
+            $table->time('end_time')->nullable();
+            $table->integer('duration')->nullable();
+            $table->string('notes')->nullable();   
+                   
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('work_sessions');
+    }
+};
